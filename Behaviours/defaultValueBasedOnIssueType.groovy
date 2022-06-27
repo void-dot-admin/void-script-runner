c9eea0b5cbeea3132ce47806e8cee69ee009ea49
unknown <freazky.andrei@gmail.com>
2022-06-27 18:39:04 +0300

    behaviour script to add default value to description based on issuetype

---
+++ Behaviours/defaultValueBasedOnIssueType.groovy
@@ -0,0 +1,37 @@
+/**
+ * Behaviour script that prefills description
+ * to a default value based on Issuetype
+ * that works only on create screen
+ */
+
+final String BUG_DESCRIPTION = """
+    * Response/Error problem:
+    {noformat}
+    {noformat}
+    * Steps to reproduce:
+    * Expected results:
+    * Actual results:
+"""
+final String EPIC_DESCRIPTION = """
+    * For what environment:
+    * UI testing (y/n):
+"""
+final String REQ_DESCRIPTION = """
+    * Please specify the environment:
+"""
+def descriptionField = getFieldById("description")
+def issueContext = issueContext.issueType.name
+
+//should work only on create screen
+if (!( getActionName() in ["Create Issue", "Create"])) {
+    return
+}
+
+switch(issueContext) {
+    case "Bug" : descriptionField.setFormValue(BUG_DESCRIPTION)
+        break
+    case "Epic" : descriptionField.setFormValue(EPIC_DESCRIPTION)
+        break
+    case "Requirement" : descriptionField.setFormValue(REQ_DESCRIPTION)
+        break
+}
