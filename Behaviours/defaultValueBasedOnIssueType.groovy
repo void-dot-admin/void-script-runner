fbc375439adfed87045c03d265e8772fe54d16ff
unknown <freazky.andrei@gmail.com>
2022-06-27 21:21:24 +0300

    removed some white spaces and fixed defaultValue formating

--- Behaviours/defaultValueBasedOnIssueType.groovy
+++ Behaviours/defaultValueBasedOnIssueType.groovy
@@ -4,26 +4,23 @@
  * that works only on create screen
  */
 
-final String BUG_DESCRIPTION = """
-    * Response/Error problem:
-    {noformat}
-    {noformat}
-    * Steps to reproduce:
-    * Expected results:
-    * Actual results:
+final String BUG_DESCRIPTION = """* Response/Error problem: 
+
+{noformat}
+* Steps to reproduce: 
+* Expected results: 
+* Actual results: 
 """
-final String EPIC_DESCRIPTION = """
-    * For what environment:
-    * UI testing (y/n):
+final String EPIC_DESCRIPTION = """* For what environment: 
+* UI testing (y/n): 
 """
-final String REQ_DESCRIPTION = """
-    * Please specify the environment:
-"""
+final String REQ_DESCRIPTION = """* Please specify the environment:"""
+
 def descriptionField = getFieldById("description")
 def issueContext = issueContext.issueType.name
 
 //should work only on create screen
-if (!( getActionName() in ["Create Issue", "Create"])) {
+if (!( getActionName() in ["Create Issue", "Create"] )) {
     return
 }
 
