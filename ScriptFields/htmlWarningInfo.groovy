860d514f7f81e395fad6d0efc7490b9b7461b6d4
void.admin <102797088+void-dot-admin@users.noreply.github.com>
2023-08-13 12:41:31 +0300

    add comments to help the user with the script config

--- ScriptFields/htmlWarningInfo.groovy
+++ ScriptFields/htmlWarningInfo.groovy
@@ -1,9 +1,14 @@
 import groovy.xml.MarkupBuilder
 
+/*
+    * Make sure to use "Test Field (multi-line)"
+    * as Template in the scripted field
+*/
+
 def writer = new StringWriter()
 def html = new MarkupBuilder (writer)
 
-
+// show the message based on IssueType & Status
 if ( issue.issueType.name == "Epic" && issue .status.name in ["To Do"] ) {
 
     // Build HTML for script field output
