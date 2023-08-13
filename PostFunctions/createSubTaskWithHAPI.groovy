13efbb28bc7bdd1fba9c13374147ca0e98af9d4f
void.admin <102797088+void-dot-admin@users.noreply.github.com>
2023-08-13 14:44:20 +0300

    Update createSubTaskWithHAPI.groovy
    A cleaner HAPI version of the code provided by https://github.com/rlander-adaptavist

--- PostFunctions/createSubTaskWithHAPI.groovy
+++ PostFunctions/createSubTaskWithHAPI.groovy
@@ -1,44 +1,35 @@
-import com.atlassian.jira.issue.AttachmentManager
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.issue.attachment.Attachment
 
-/**
-    *Post-function script that creates a Sub-task issue type with name "Sub-task"
-    *copy values & attachments from parent issue type
-    *This version is updated to use the HAPI feature from Scriptrunner
-*/
+def parentIssue = issue
 
-// the issue type name for the new issue* must be Sub-task to work
-final issueTypeName = "Sub-task"
-
-// get the logged in user
-def loggedInUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser
-
-// get the field values from parent
-def priorityName = issue.getPriority().name
-def parentComponents = issue.getComponents().toListString()
-def parentAssignee = issue.getAssignee()
-def parentTaskChoiceValue = issue.getCustomFieldValue("Task choice")
-def parentIssue = ComponentAccessor.issueManager.getIssueByCurrentKey(issue.key)
-def parentSummary = issue.getSummary()
-def parentDescription = issue.getDescription()
-
-Long[] componentIds = issue.getComponents()*.id
-
-// create the sub-task with HAPI
-def subtask = parentIssue.createSubTask(issueTypeName) {
-    setReporter(loggedInUser)
-    setAssignee(parentAssignee)
-    setSummary("Sub-task for: ${parentSummary}")
-    setDescription(parentDescription)
-    setPriority(priorityName)
-    setCustomFieldValue("Task choice", parentTaskChoiceValue)
-    setComponents { set(componentIds) }
+if (parentIssue.isSubTask()) {
+    return
 }
-        
-// copy parent attachments to sub-task
-AttachmentManager attachmentManager = ComponentAccessor.getAttachmentManager()
-Collection<Attachment> parentIssueAttachments = parentIssue.getAttachments()
-    parentIssueAttachments.each{
-        attachmentManager.copyAttachment(it, loggedInUser, subtask.key)
+
+def subTask = parentIssue.createSubTask('Sub-task') {
+    setSummary("Sub-task for: ${parentIssue.summary}")
+    setDescription(parentIssue.description)
+    setPriority(parentIssue.priority)
+
+    def taskChoice = parentIssue.getCustomFieldValue('Task choice')
+
+    if (taskChoice) {
+        setCustomFieldValue('Task choice', taskChoice)
+    }
+
+    setAssignee {
+        if (parentIssue.assignee) {
+            set(parentIssue.assignee)
+        } else {
+            automatic()
+        }
+    }
+
+    setComponents {
+        parentIssue.components.each { add(it) }
+    }
+
 }
+
+def attachmentManager = ComponentAccessor.getAttachmentManager()
+attachmentManager.copyAttachments(parentIssue, Users.loggedInUser, subTask.key)
