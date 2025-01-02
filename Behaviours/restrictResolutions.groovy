0f0b44aabbc3862ff8f5e6c896cde9e3c84fadc4
void.admin <102797088+void-dot-admin@users.noreply.github.com>
2025-01-02 22:11:02 +0200

    Create restrictResolutions.groovy

---
+++ Behaviours/restrictResolutions.groovy
@@ -0,0 +1,23 @@
+import static com.atlassian.jira.issue.IssueFieldConstants.RESOLUTION
+
+/*
+    *This Initialiser behaviour will restrict the resolutions
+    *based on the Issue Types
+    *please place this script as an Initialiser in your behaviour
+*/
+
+def resolution = getFieldById(RESOLUTION)
+// get the Issue Type name from the issue where you trigger the transition
+def issueType = issueContext.getIssueType().name
+
+// only run if the transition is "Done"
+if (getActionName() == "Done") {
+    // based on the Issue Type name, restrict resolution values
+    if (issueType == "Task"){
+        resolution.setFieldOptions(["Done", "Canceled"])
+    }else if (issueType == "Bug"){
+        resolution.setFieldOptions(["Done", "Cannot Reproduce","Invalid"])
+    }
+    
+}
+
